fd3c374a51b091dddf57f9af54014303f14c67af
MostafaZayed72 <zayed8646@gmail.com>
2024-02-06 13:29:43 +0200

    n

--- components/homeTable.vue
+++ components/homeTable.vue
@@ -44,9 +44,24 @@
         ></div>
         <div class="select flex flex-col items-end justify-center gap-2">
           <h1 class="text-end mr-20 mt-2">اختر التخصص و وسيلة التواصل</h1>
-<v-select class="w-80 mr-20" v-model="selectedItem"
-          :items="items"
-          ></v-select>
+          <v-select
+            class="w-80 mr-20 flex"
+            :items="mylist"
+            item-title="name"
+            item-value="id"
+            v-model="selectedItem"
+          >
+            <template #item="{ item, props }">
+              <v-list-item v-bind="props">
+                <template #title>
+                  <div class="flex items-center justify-end gap-2">
+                    <h1>{{ item.raw.name }}</h1>
+                    <img :src="item.raw.image" />
+                  </div>
+                </template>
+              </v-list-item>
+            </template>
+          </v-select>
         </div>
       </div>
     </div>
@@ -58,6 +73,149 @@ const medical_menu = ref(false);
 const show_medical = () => {
   medical_menu.value = !medical_menu.value;
 };
-const selectedItem = ref("جراحة");
-const items = ["Arabic / اللغة العربية" , "English / اللغة الإنجليزية"];
+const selectedItem = ref("التخصصات الطبية");
+const mylist = [
+  {
+    id: 0,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/pediatrics.svg",
+    name: "طب أطفال",
+  },
+  {
+    id: 1,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/svgexport-6(93).svg",
+    name: "أنف وأذن وحنجرة",
+  },
+  {
+    id:2 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Interal.svg",
+    name: "باطنة",
+  },
+  {
+    id:3 ,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Genecology.svg ",
+    name: "نساء وتوليد",
+  },
+  {
+    id:4 ,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/physic.svg ",
+    name: "علاج طبيعي",
+  },
+  {
+    id:5 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Family(1).svg",
+    name: "طب الأسرة",
+  },
+  {
+    id:6 ,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Orology.svg ",
+    name: "مسالك بولية",
+  },{
+    id:7,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Ophtamology.svg ",
+    name: "عيون",
+  },
+  {
+    id:8 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Hepatology.svg",
+    name: "الكبد والجهازالهضمي",
+  },{
+    id: 9,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/kidney-organ.svg",
+    name: "كلى",
+  },
+  {
+    id: 10,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/PlasticSurgery.svg",
+    name: "جراحة تجميل",
+  },{
+    id: 11,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/ObesityandLaparoscopicSurgery.svg",
+    name: "جراحة سمنة",
+  },
+  {
+    id:12 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/svgexport-6(90).svg",
+    name: "جلدية وتناسلية",
+  },{
+    id: 13,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Surgery.svg ",
+    name: "جراحة",
+  },
+  {
+    id: 14,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Orthopedics-1.svg",
+    name: "عظام",
+  },{
+    id: 15,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/General.svg",
+    name: "ممارس عام",
+  },
+  {
+    id: 16,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Old.svg ",
+    name: "طب الشيخوخة",
+  },{
+    id:17 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/svgexport-6(89).svg",
+    name: "قلب",
+  },
+  {
+    id:18 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/svgexport-6(92).svg",
+    name: "أسنان",
+  },{
+    id:19 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Neurology-1.svg",
+    name: "مخ وأعصاب",
+  },
+  {
+    id:20 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Oncology(Tumor).svg",
+    name: "أورام",
+  },{
+    id:21 ,
+    image:
+      "https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/lab.svg ",
+    name: "تحاليل وأشعة",
+  },
+  {
+    id:22 ,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/ChestandRespiratory.svg",
+    name: "صدر وجهاز تنفسي",
+  },
+  {
+    id:23 ,
+    image:
+      " ",
+    name: " ",
+  },
+  {
+    id: 24,
+    image:
+      " https://cdn.statically.io/gh/AhmedMSoliman160/20230301v01/main/assets/images/icons/Hematology1.svg",
+    name: "أمراض الدم",
+  },
+];
 </script>
