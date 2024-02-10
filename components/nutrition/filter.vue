b1c9d1f6f9d38e3aae3abb4b2c11626c09c391f5
MostafaZayed72 <zayed8646@gmail.com>
2024-02-10 20:03:03 +0200

    n

--- components/nutrition/filter.vue
+++ components/nutrition/filter.vue
@@ -28,61 +28,84 @@
         </div>
       </div>
 
-      <div class="card mb-10">
+      <div class="card mb-10 w-100">
         <v-row
           ><v-col
             cols="12"
-            class="mt-10 flex flex-col gap-20 py-10 rounded-md flex flex-row-reverse items-center justify-start gap-2 w-100 bg-white"
+            class="mt-10 flex gap-20 py-10 rounded-md flex flex-col md:flex-row-reverse items-center justify-start gap-0 md:gap-2 w-100 bg-white"
             v-for="item in sortedData"
             :key="item.image"
           >
-            <img
-              :src="item.image"
-              alt=""
-              class="w-28 h-28 rounded-full"
-              style="border: 1px solid rgb(133, 128, 128)"
-            />
-            <div class="w-32 text text-center">
-              <nuxt-link
-                to=""
-                class="cursor-pointer font-bold text-xl"
-                style="text-decoration: underline"
-              >
-                د/ {{ item.name }}</nuxt-link
-              >
-              <h1 class="text-gray-700 text-sm mt-1">{{ item.subtitle }}</h1>
-              <div class="star text-center">
-                {{ item.rate }}<i class="fa-solid fa-star ml-1 mt-2"></i>
-                <div class="experts flex justify-center gap-1">
-                  <h1>سنوات خبرة</h1>
-                  <h1>{{ item.experts }}</h1>
-                </div>
-              </div>
-            </div>
-            <div class="w-32 price text-center mr-10">
-              <h1 class="mb-2" style="text-decoration: underline">الأسعار</h1>
-              <h1 style="direction: rtl">{{ item.value }} جنيه/كشف</h1>
-              <h1 style="direction: rtl">{{ item.value_2 }} جنيه/استشارة</h1>
-            </div>
-            <div class="next-time flex gap-4 mr-10">
-              <div class="text-center">
-                <h1 class="mb-2">أقرب موعد</h1>
-                <div
-                  class="colored-text w-100 rounded-md"
-                  style="border: 1px solid rgb(201, 195, 195)"
+            <div class="text text-center flex flex-row-reverse">
+              <img
+                :src="item.image"
+                alt=""
+                class="rounded-full"
+                style="
+                  border: 1px solid rgb(133, 128, 128);
+                  width: 150px;
+                  height: 150px;
+                "
+              />
+              <div class="text flex flex-col items-center justify-center mr-4">
+                <nuxt-link
+                  to=""
+                  class="cursor-pointer font-bold text-xl"
+                  style="text-decoration: underline"
                 >
-                  <div
-                    class="date flex justify-around items-center px-2 py-1 bg-yellow  gap-10"
-                  >
-                    <h1 style="direction: rtl;">{{ item.next_day }}</h1>
-                    <h1>:التاريخ</h1>
+                  د/ {{ item.name }}</nuxt-link
+                >
+                <h1 class="text-gray-700 text-sm mt-1">{{ item.subtitle }}</h1>
+                <div class="star text-center">
+                  {{ item.rate }}<i class="fa-solid fa-star ml-1 mt-2"></i>
+                  <div class="experts flex justify-center gap-1">
+                    <h1>سنوات خبرة</h1>
+                    <h1>{{ item.experts }}</h1>
                   </div>
-                  <h1 class="mt-4" style="direction: rtl">
-                    {{ item.next_time }}
-                  </h1>
                 </div>
               </div>
-              <div class="line h-32 bg-gray-300" style="width: 2px"></div>
+            </div>
+            <div
+              class="flex md:flex-row-reverse flex-col items-center justify-center mr-16"
+            >
+              <div class="price text-center">
+                <h1 class="mb-2 mt-4" style="text-decoration: underline">
+                  الأسعار
+                </h1>
+                <h1 style="direction: rtl">{{ item.value }} جنيه/كشف</h1>
+                <h1 style="direction: rtl">{{ item.value_2 }} جنيه/استشارة</h1>
+              </div>
+              <div class="next-time flex gap-4">
+                <div class="reservation flex flex-col gap-4 mr-32 mt-14 text-center">
+                  <nuxt-link class="px-2 py-1 bg-green-400 rounded-md md:px-10" to=""
+                    >احجز موعد</nuxt-link
+                  >
+                  <nuxt-link class="r px-2 py-1 rounded-md md:px-10" to="" style="border: 1px solid;"
+                    >مواعيد أخرى</nuxt-link
+                  >
+                </div>
+                <div class="text-center">
+                  <h1 class="mb-2 mt-6 md:mt-1">أقرب موعد</h1>
+                  <div
+                    class="colored-text w-100 rounded-md"
+                    style="border: 1px solid rgb(201, 195, 195)"
+                  >
+                    <div
+                      class="date flex justify-around items-center px-2 py-1 bg-yellow gap-10"
+                    >
+                      <h1 style="direction: rtl">{{ item.next_day }}</h1>
+                      <h1>:التاريخ</h1>
+                    </div>
+                    <h1 class="mt-4" style="direction: rtl">
+                      {{ item.next_time }}
+                    </h1>
+                  </div>
+                </div>
+                <div
+                  class="line h-40 md:bg-gray-300 bg-white-100 md:mr-32"
+                  style="width: 2px"
+                ></div>
+              </div>
             </div> </v-col
         ></v-row>
       </div>
@@ -119,28 +142,30 @@ const data = ref([
     rate: 4.8,
     image:
       "https://img.freepik.com/free-photo/portrait-attractive-male-doctor_329181-622.jpg?size=626&ext=jpg&ga=GA1.1.1483280730.1707239230&semt=sph",
-  },{
-    name: "أحمد متولي",
-    value: 5,
+  },
+  {
+    name: "مصطفى زيدان",
+    value: 300,
     subtitle: "أخصائي تغذية",
-    experts: 4,
-    value_2: 2,
-    next_day: "10 FEB",
-    next_time: "10.30 م - 11.30 م",
-    rate: 4.5,
+    experts: 6,
+    value_2: 150,
+    next_day: "23 FEB",
+    next_time: "10 م - 9 م",
+    rate: 4.1,
     image:
-      " https://img.freepik.com/free-photo/smiling-doctor-with-strethoscope-isolated-grey_651396-974.jpg?w=740&t=st=1707507653~exp=1707508253~hmac=8b5f43967ce74599905c90e60741ccbefbb150224a9f7740d8aad016427b4d22",
-  },{
-    name: "أحمد متولي",
-    value: 5,
-    subtitle: "أخصائي تغذية",
-    experts: 4,
-    value_2: 2,
-    next_day: "10 FEB",
-    next_time: "10.30 م - 11.30 م",
-    rate: 4.5,
+      " https://img.freepik.com/free-photo/doctor-with-his-arms-crossed-white-background_1368-5790.jpg?w=740&t=st=1707587082~exp=1707587682~hmac=2a559f9ccf3e28272baefb518d525e4cc6863e6f718ecf699a582f21e85686b7",
+  },
+  {
+    name: "محمود طعيمة",
+    value: 500,
+    subtitle: "استشاري تغذية",
+    experts: 7,
+    value_2: 200,
+    next_day: "27 FEB",
+    next_time: "3.30 م - 4.30 م",
+    rate: 4.9,
     image:
-      " https://img.freepik.com/free-photo/smiling-doctor-with-strethoscope-isolated-grey_651396-974.jpg?w=740&t=st=1707507653~exp=1707508253~hmac=8b5f43967ce74599905c90e60741ccbefbb150224a9f7740d8aad016427b4d22",
+      "https://img.freepik.com/free-photo/happy-doctor-wearing-glasses-presenting-something_329181-616.jpg?w=740&t=st=1707587190~exp=1707587790~hmac=dc703229e05edec6b021d921c67e24ab76fcae3cf0d7a5ffbd791877354645b2",
   },
 ]);
 
