8fbcaddc70398babe14d512397e18e21b5a6c49d
MostafaZayed72 <zayed8646@gmail.com>
2024-02-11 19:30:18 +0200

    n

--- pages/medical/index.vue
+++ pages/medical/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <QuestionsNav /><experts />
+   <div class="sticky top-0 shadow-lg z-30 bg-white pb-4"><QuestionsNav /></div><experts />
   <div><MedicalAvailableDoctors /></div>
   <div><medicalFilter /></div>
 </template>
