88a3865c4574dc0239c8ab19868888ee196c7c04
MostafaZayed72 <zayed8646@gmail.com>
2024-02-16 21:23:37 +0200

    n

--- components/secondaryNav.vue
+++ components/secondaryNav.vue
@@ -24,21 +24,23 @@ function showMenu() {
       mdi-menu
     </v-icon>
     <div class="links ml-10 flex items-center gap-4">
-      <div
-        class="ask cursor-pointer py-1 flex justify-center items-center gap-2"
-        style="width: 200px; background-color: rgb(0, 51, 51)"
-      >
-        <h1 class="text-white">
-          اسأل خبير
-        </h1>
-        <i class="fa-regular fa-comment text-slate-300" />
-      </div>
+      <nuxt-link
+      to="/nutrition"
+      class="ask cursor-poi py-1 flex justify-center items-center gap-2"
+      style="width: 200px; background-color: rgb(0, 51, 51)"
+    >
+      <h1 class="text-white">
+        اسأل خبير
+      </h1>
+      <i class="fa-regular fa-comment text-slate-300" />
+    </nuxt-link>
       <h1 class="text-lg">
         أو
       </h1>
       <div
         class="reservation cursor-pointer py-1 flex justify-center items-center gap-2"
         style="width: 200px; background-color: rgb(0, 51, 51)"
+        @click="dialog = !dialog"
       >
         <h1 class="text-white">
           حجز استشارة
@@ -55,7 +57,7 @@ function showMenu() {
     >
   </div>
   <v-container
-    v-if="menu"
+    v-if="menu || dialog"
     class="menu bg-gray-300 z-50 drop-menu mx-auto"
     style="transform: translateY(-10px)"
   >
@@ -192,15 +194,16 @@ function showMenu() {
     class="bottom-links z-10 w-4/5 md:w-3/5 mx-auto flex items-center justify-center gap-4 h-10 rounded-sm sticky top-20"
     style="background-color: rgb(173, 177, 176)"
   >
-    <div
-      class="ask cursor-pointer py-1 flex justify-center items-center gap-2"
+    <nuxt-link
+      to="/nutrition"
+      class="ask cursor-poi py-1 flex justify-center items-center gap-2"
       style="width: 200px; background-color: rgb(0, 51, 51)"
     >
       <h1 class="text-white">
         اسأل خبير
       </h1>
       <i class="fa-regular fa-comment text-slate-300" />
-    </div>
+    </nuxt-link>
     <h1 class="text-lg">
       أو
     </h1>
